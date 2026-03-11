effb0fae9a56f1ead50795a1cba4f2876fd56f4d
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-11 20:53:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ReadingItem[] books =
+        {
+            new ReadingItem("Atomic Habits", "James Clear", 600, true),
+            new ReadingItem("Clean Code", "Robert C. Martin", 450, false),
+            new ReadingItem("Deep Learning", "Ian Goodfellow", 520, false),
+            new ReadingItem("Rich Dad Poor Dad", "Robert Kiyosaki", 300, true)
+        };
+
+        foreach (ReadingItem book in books)
+        {
+            book.ShowBookDetails();
+        }
+    }
+}
---
+++ ReadingItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class ReadingItem
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public double Price { get; set; }
+    public bool IsBestseller { get; set; }
+    public ReadingItem(string title, string author, double price, bool bestseller)
+    {
+        Title = title;
+        Author = author;
+        Price = price;
+        IsBestseller = bestseller;
+    }
+    public double GetDiscountedPrice()
+    {
+        double finalPrice = Price;
+
+        if (IsBestseller)
+        {
+            Console.WriteLine($"{Title} is a bestseller. Applying 10% discount.");
+            finalPrice = finalPrice - (finalPrice * 0.10);
+        }
+
+        if (Price > 500)
+        {
+            Console.WriteLine($"{Title} is priced over 500. Applying additional 5% discount.");
+            finalPrice = finalPrice - (finalPrice * 0.05);
+        }
+        return finalPrice;
+    }
+    public void ShowBookDetails()
+    {
+        Console.WriteLine("\nTitle: " + Title);
+        Console.WriteLine("Author: " + Author);
+        Console.WriteLine($"Original Price: ${Price:F2}");
+
+        double discounted = GetDiscountedPrice();
+
+        Console.WriteLine($"Final Discounted Price: ${discounted:F2}");
+    }
+}
