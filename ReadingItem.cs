3d1361facacf4d3bae475d9bd43606437d9e3ec1
AnkitaHeralagi <126943633+AnkitaHeralagi@users.noreply.github.com>
2026-03-13 20:11:24 +0530

    Updated

--- ReadingItem.cs
+++ ReadingItem.cs
@@ -1,43 +1,44 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-class ReadingItem
+namespace Book_Discount_Calculator
 {
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public double Price { get; set; }
-    public bool IsBestseller { get; set; }
-    public ReadingItem(string title, string author, double price, bool bestseller)
+    internal class ReadingItem
     {
-        Title = title;
-        Author = author;
-        Price = price;
-        IsBestseller = bestseller;
-    }
-    public double GetDiscountedPrice()
-    {
-        double finalPrice = Price;
-
-        if (IsBestseller)
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public double Price { get; set; }
+        public bool IsBestseller { get; set; }
+        public ReadingItem(string title, string author, double price, bool bestseller)
         {
-            Console.WriteLine($"{Title} is a bestseller. Applying 10% discount.");
-            finalPrice = finalPrice - (finalPrice * 0.10);
+            Title = title;
+            Author = author;
+            Price = price;
+            IsBestseller = bestseller;
         }
-
-        if (Price > 500)
+        public double GetDiscountedPrice()
         {
-            Console.WriteLine($"{Title} is priced over 500. Applying additional 5% discount.");
-            finalPrice = finalPrice - (finalPrice * 0.05);
+            double finalPrice = Price;
+            if (IsBestseller)
+            {
+                Console.WriteLine($"{Title} is a bestseller. Applying 10% discount.");
+                finalPrice = finalPrice - (finalPrice * 0.10);
+            }
+            if (Price > 500)
+            {
+                Console.WriteLine($"{Title} is priced over 500. Applying additional 5% discount.");
+                finalPrice = finalPrice - (finalPrice * 0.05);
+            }
+            return finalPrice;
         }
-        return finalPrice;
-    }
-    public void ShowBookDetails()
-    {
-        Console.WriteLine("\nTitle: " + Title);
-        Console.WriteLine("Author: " + Author);
-        Console.WriteLine($"Original Price: ${Price:F2}");
-
-        double discounted = GetDiscountedPrice();
-
-        Console.WriteLine($"Final Discounted Price: ${discounted:F2}");
+        public void ShowBookDetails()
+        {
+            Console.WriteLine("\nTitle: " + Title);
+            Console.WriteLine("Author: " + Author);
+            Console.WriteLine($"Original Price: ${Price:F2}");
+            double discounted = GetDiscountedPrice();
+            Console.WriteLine($"Final Discounted Price: ${discounted:F2}");
+        }
     }
 }
